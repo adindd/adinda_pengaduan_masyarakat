7a73beaa3d77d556ff943202d4e29de503a1da25
adind <111264781+adindd@users.noreply.github.com>
2023-08-02 11:46:24 +0700

    Add files via upload

---
+++ LAPORAN PENGADUAN MASYARAKAT/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <body bgcolor="white">
+
+    <div>
+      <center><p><big><big><big><b>LOGIN</b></big></big></big></p></center>
+    <div>
+    <img src="login.png" 
+    style="display:block; margin:auto; widht:250px; height:250px;">
+    </div>
+    <div>
+    <center>
+    <form action= "registrasi.php" method="post">
+        <p>Email: <input type="text" name="email" id=""></p>
+        <p>Password: <input type="text" name="password" id =""></p>
+        <button type="batal">Batal</button>
+        <button type="submit">Login</button>
+        <p>Belum punya akun?
+            <a href="registrasi.php">Daftar disini</a>
+        </p>
+    </form>
+    </center>
+    </div>
+</body>
+</html>
