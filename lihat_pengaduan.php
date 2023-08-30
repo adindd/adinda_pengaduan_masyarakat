faefb6e9188063f4544e71fd1d7bb0a32abc778d
adind <111264781+adindd@users.noreply.github.com>
2023-08-30 17:23:19 +0700

    Update lihat_pengaduan.php

--- lihat_pengaduan.php
+++ lihat_pengaduan.php
@@ -1,61 +1,53 @@
-<?php
-session_start();
-// if (!isset($_SESSION['nama']))
-// {
-//     die("Anda belum Login");
-// }
-?>
-
-<?php
-
-$koneksi = new PDO("mysql:host=localhost;dbname=laporanpengaduanmasyarakat","root", "");
-
-$query = $koneksi->query("select * from pengaduan");
-$data = $query->fetchAll();
-
-
-//if($koneksi){
-//  echo "koneksi berhasil";
-//}else{
-  //echo "koneksi gagal";
-//}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bs/css/bootstrap.min.css">
-    <title>lihat pengaduan</title>
+  <title>Document</title>
 </head>
 <body>
-</body>
-<body>
+  
+<?php
+    $koneksi= mysqli_connect("localhost", "root", "", "laporanpengaduanmasyarakat");
+    
+  
+    $query = $koneksi->query("SELECT * FROM pengaduan");
+   
+?>
+
 <nav class="navbar navbar-expand-lg" style="background-color:#8CABFF;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><font face="" color="black">Pengaduan Masyarakat</font></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+    <a class="navbar-brand" href="#">Pengaduan Masyarakat</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-    <form action="logout .php" method="post">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="masyarakat.php"><font face="" color="black">home</font></a>
+          <a class="nav-link active" aria-current="page" href="halaman.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="buat_pengaduan.php"><font face="" color="black">buat pengaduan</font></a>
+          <a class="nav-link" href="id_pengaduan.php">Laporan</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="lihat_pengaduan.php"><font face="" color="black">lihat pengaduan</font></a>
+          <a class="nav-link" href="lihat_pengaduan.php">Lihat</a>
         </li>
       </ul>
+      </div>
+      <a class="nav-link" href="login.php"><button class="btn btn-outline-success" onclick="confirmLogout()"><font face="" color="black">Logout</font></button></a>
+  </div>
     </div>
-        <button class="btn btn-outline-success" type="submit" href="logout.php"><font face="" color="black">logout</font></button>
   </div>
 </nav>
+</body>
+
+<body>
 <br><br><br>
+<a class="nav-link" href="id_pengaduan.php"><button class="btn btn-outline-success"><font face="" color="black">tambahkan</font></button></a>
 <div class="container">
 <table class="table table-primary">
   <thead>
@@ -64,24 +56,24 @@ $data = $query->fetchAll();
       <th scope="col">Id</th>
       <th scope="col">Tanggal</th>
       <th scope="col">Nik</th>
-      <th scope="col">Isi</th>
+      <th scope="col">Isi Laporan</th>
       <th scope="col">Foto</th>
       <th scope="col">Status</th>
     </tr>
   </thead>
   <tbody>
+  <?php while( $data = mysqli_fetch_array($query)) : ?>
     <tr>
       <th scope="row">1</th>
-      <td>11111</td>
-      <td>06-08-2023</td>
-      <td>2122039</td>
-      <td>Kebanjiran</td>
-      <td> 
-      <img src="kebanjiran.jpg" style="width:80px; height:80px;"/>
-      </td>
-      <td>Terkirim</td>
-      </td>
+      <td><?= $data['id_pengaduan'] ?></td>
+      <td><?= $data['tgl_pengaduan'] ?></td>
+      <td><?= $data['nik'] ?></td>
+      <td><?= $data['isi_laporan'] ?></td>
+      <td><?= $data['foto']?></td>
+      <td><?= $data['status']?></td>
     </tr>
+    <?php endwhile ?>
   </tbody>
 </table>
-</div>
+</body>
+</html>
